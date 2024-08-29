96d102ca0020fbf6789999fa0a3af388a0220fce
dev-muuguu <dev.muuguu@gmail.com>
2024-08-29 17:43:47 +0800

    ts-linter

--- app.ts
+++ app.ts
@@ -1,10 +1,6 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import swaggerUi from "swagger-ui-express";
-// import swaggerSpec from "./utils/swagger";
-// import authRouter from "./api/auth/router";
-// import productRouter from "./api/product/router";
 
 const app = express();
 
@@ -12,18 +8,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log(`${new Date().toUTCString()}:${req.method} ${req.url}`);
-  next();
-});
+const PORT: number | string = process.env.PORT || 8080;
 
-// Serve Swagger UI
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT);
 
 export default app;
